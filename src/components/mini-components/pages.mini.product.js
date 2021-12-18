1b2f18603601d09c1155ee7ec8e662d3df3f6988
naziiriah <naziistry7@gmail.com>
2021-12-18 14:22:10 +0100

    media

--- src/components/mini-components/pages.mini.product.js
+++ src/components/mini-components/pages.mini.product.js
@@ -7,7 +7,7 @@ const MiniProductSection = ({image, name}) => {
     return ( 
         <Box as="section" width={["20rem", "20rem", "10rem", "20rem"]} height="31rem" margin="auto">
             <Image src={image} alt={name} marginTop="-5rem"/>
-            <Box as ="h2" textAlign="center" marginTop="1rem" fontSize="23px" fontWeight="600">{name}</Box>
+            <Box as ="h2" textAlign="center" marginTop={["0.5rem" ,"0.5rem", "1rem", "1rem"]} fontSize="23px" fontWeight="600">{name}</Box>
             <Box marginLeft ="3rem">
             <Buttons text="see products"color="#D87D4A" width="100%"  stateName={name}/>
             </Box>
